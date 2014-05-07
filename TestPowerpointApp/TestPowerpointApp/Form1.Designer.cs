e6f4e5baa4d7246cba9b59b745a69ee96330cdaf
Joel Matton <joelmatton@hotmail.com>
2014-05-07 14:20:28 -0400

    Created basic form that will open up a powerpoint and display slides

--- TestPowerpointApp/TestPowerpointApp/Form1.Designer.cs
+++ TestPowerpointApp/TestPowerpointApp/Form1.Designer.cs
@@ -1,6 +1,6 @@
 ﻿namespace TestPowerpointApp
 {
-    partial class Form1
+    partial class frmTestPowerPoint
     {
         /// <summary>
         /// Required designer variable.
@@ -28,12 +28,100 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.btnCheckIsRunning = new System.Windows.Forms.Button();
+            this.btnFirst = new System.Windows.Forms.Button();
+            this.btnNext = new System.Windows.Forms.Button();
+            this.btnPrevious = new System.Windows.Forms.Button();
+            this.btnLast = new System.Windows.Forms.Button();
+            this.btnOpenPptDoc = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // btnCheckIsRunning
+            // 
+            this.btnCheckIsRunning.Location = new System.Drawing.Point(12, 12);
+            this.btnCheckIsRunning.Name = "btnCheckIsRunning";
+            this.btnCheckIsRunning.Size = new System.Drawing.Size(192, 23);
+            this.btnCheckIsRunning.TabIndex = 0;
+            this.btnCheckIsRunning.Text = "Check Powerpoint Is Running";
+            this.btnCheckIsRunning.UseVisualStyleBackColor = true;
+            this.btnCheckIsRunning.Click += new System.EventHandler(this.btnCheckIsRunning_Click);
+            // 
+            // btnFirst
+            // 
+            this.btnFirst.Location = new System.Drawing.Point(12, 54);
+            this.btnFirst.Name = "btnFirst";
+            this.btnFirst.Size = new System.Drawing.Size(75, 23);
+            this.btnFirst.TabIndex = 1;
+            this.btnFirst.Text = "First Page";
+            this.btnFirst.UseVisualStyleBackColor = true;
+            this.btnFirst.Click += new System.EventHandler(this.btnFirst_Click);
+            // 
+            // btnNext
+            // 
+            this.btnNext.Location = new System.Drawing.Point(103, 54);
+            this.btnNext.Name = "btnNext";
+            this.btnNext.Size = new System.Drawing.Size(75, 23);
+            this.btnNext.TabIndex = 2;
+            this.btnNext.Text = "Next Page";
+            this.btnNext.UseVisualStyleBackColor = true;
+            this.btnNext.Click += new System.EventHandler(this.btnNext_Click);
+            // 
+            // btnPrevious
+            // 
+            this.btnPrevious.Location = new System.Drawing.Point(197, 54);
+            this.btnPrevious.Name = "btnPrevious";
+            this.btnPrevious.Size = new System.Drawing.Size(75, 23);
+            this.btnPrevious.TabIndex = 3;
+            this.btnPrevious.Text = "Previous Page";
+            this.btnPrevious.UseVisualStyleBackColor = true;
+            this.btnPrevious.Click += new System.EventHandler(this.btnPrevious_Click);
+            // 
+            // btnLast
+            // 
+            this.btnLast.Location = new System.Drawing.Point(278, 54);
+            this.btnLast.Name = "btnLast";
+            this.btnLast.Size = new System.Drawing.Size(75, 23);
+            this.btnLast.TabIndex = 4;
+            this.btnLast.Text = "Last Page";
+            this.btnLast.UseVisualStyleBackColor = true;
+            this.btnLast.Click += new System.EventHandler(this.btnLast_Click);
+            // 
+            // btnOpenPptDoc
+            // 
+            this.btnOpenPptDoc.Location = new System.Drawing.Point(210, 12);
+            this.btnOpenPptDoc.Name = "btnOpenPptDoc";
+            this.btnOpenPptDoc.Size = new System.Drawing.Size(192, 23);
+            this.btnOpenPptDoc.TabIndex = 5;
+            this.btnOpenPptDoc.Text = "open a ppt";
+            this.btnOpenPptDoc.UseVisualStyleBackColor = true;
+            this.btnOpenPptDoc.Click += new System.EventHandler(this.btnOpenPptDoc_Click);
+            // 
+            // frmTestPowerPoint
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.Text = "Form1";
+            this.ClientSize = new System.Drawing.Size(587, 143);
+            this.Controls.Add(this.btnOpenPptDoc);
+            this.Controls.Add(this.btnLast);
+            this.Controls.Add(this.btnPrevious);
+            this.Controls.Add(this.btnNext);
+            this.Controls.Add(this.btnFirst);
+            this.Controls.Add(this.btnCheckIsRunning);
+            this.Name = "frmTestPowerPoint";
+            this.Text = "PowerPointTester";
+            this.Load += new System.EventHandler(this.frmTestPowerPoint_Load);
+            this.ResumeLayout(false);
+
         }
 
         #endregion
+
+        private System.Windows.Forms.Button btnCheckIsRunning;
+        private System.Windows.Forms.Button btnFirst;
+        private System.Windows.Forms.Button btnNext;
+        private System.Windows.Forms.Button btnPrevious;
+        private System.Windows.Forms.Button btnLast;
+        private System.Windows.Forms.Button btnOpenPptDoc;
     }
 }
 
--- TestPowerpointApp/TestPowerpointApp/Form1.cs
+++ TestPowerpointApp/TestPowerpointApp/Form1.cs
@@ -7,14 +7,197 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using System.Runtime.InteropServices;
+using PPt = Microsoft.Office.Interop.PowerPoint;
+using Microsoft.Office.Core;
 
 namespace TestPowerpointApp
 {
-    public partial class Form1 : Form
+    public partial class frmTestPowerPoint : Form
     {
-        public Form1()
+        PPt._Application pptApplication;
+        PPt.Presentation presentation;
+        PPt.Presentation New_Presentation;
+        PPt.Slides slides;
+        int slidescount;
+        PPt.Slide slide;
+        int slideIndex;
+
+        public frmTestPowerPoint()
         {
             InitializeComponent();
+        }
+
+        private void frmTestPowerPoint_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btnCheckIsRunning_Click(object sender, EventArgs e)
+        {
+
+
+            pptApplication = new PPt.Application();
+            try
+            {
+                pptApplication = null;
+                // Get Running PowerPoint Application object 
+                pptApplication = Marshal.GetActiveObject("PowerPoint.Application") as PPt.Application;
+                
+                // Get PowerPoint application successfully, then set control button enable 
+                this.btnFirst.Enabled = true;
+                this.btnNext.Enabled = true;
+                this.btnPrevious.Enabled = true;
+                this.btnLast.Enabled = true;
+            if (pptApplication != null)
+            {
+
+                // Get Presentation Object 
+                presentation = pptApplication.ActivePresentation;
+                // Get Slide collection object 
+                slides = presentation.Slides;
+                // Get Slide count 
+                slidescount = slides.Count;
+                // Get current selected slide  
+                try
+                {
+                    // Get selected slide object in normal view 
+                    slide = slides[pptApplication.ActiveWindow.Selection.SlideRange.SlideNumber];
+                }
+                catch
+                {
+                    // Get selected slide object in reading view 
+                    slide = pptApplication.SlideShowWindows[1].View.Slide;
+                } 
+            } 
+                 
+             }
+            catch
+            {
+                MessageBox.Show("Please Run PowerPoint Firstly", "Error", MessageBoxButtons.OKCancel, MessageBoxIcon.Error);
+            } 
+
+        }
+
+        private void btnFirst_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                // Call Select method to select first slide in normal view 
+                slides[1].Select();
+                slide = slides[1];
+            }
+            catch
+            {
+                // Transform to first page in reading view 
+                pptApplication.SlideShowWindows[1].View.First();
+                slide = pptApplication.SlideShowWindows[1].View.Slide;
+            } 
+        }
+
+        private void btnLast_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                slides[slidescount].Select();
+                slide = slides[slidescount];
+            }
+            catch
+            {
+                pptApplication.SlideShowWindows[1].View.Last();
+                slide = pptApplication.SlideShowWindows[1].View.Slide;
+            } 
+        }
+
+        private void btnNext_Click(object sender, EventArgs e)
+        {
+            slideIndex = slide.SlideIndex + 1;
+            if (slideIndex > slidescount)
+            {
+                MessageBox.Show("It is already last page");
+            }
+            else
+            {
+                try
+                {
+                    slide = slides[slideIndex];
+                    slides[slideIndex].Select();
+                }
+                catch
+                {
+                    pptApplication.SlideShowWindows[1].View.Next();
+                    slide = pptApplication.SlideShowWindows[1].View.Slide;
+                }
+            } 
+        }
+
+        private void btnPrevious_Click(object sender, EventArgs e)
+        {
+            slideIndex = slide.SlideIndex - 1;
+            if (slideIndex >= 1)
+            {
+                try
+                {
+                    slide = slides[slideIndex];
+                    slides[slideIndex].Select();
+                }
+                catch
+                {
+                    pptApplication.SlideShowWindows[1].View.Previous();
+                    slide = pptApplication.SlideShowWindows[1].View.Slide;
+                }
+            }
+            else
+            {
+                MessageBox.Show("It is already Fist Page");
+            } 
+        }
+
+        private void btnOpenPptDoc_Click(object sender, EventArgs e)
+        {
+            pptApplication = new PPt.Application();
+            try
+            {
+                pptApplication = null;
+                // Get Running PowerPoint Application object 
+                pptApplication = Marshal.GetActiveObject("PowerPoint.Application") as PPt.Application;
+
+                // Get PowerPoint application successfully, then set control button enable 
+                this.btnFirst.Enabled = true;
+                this.btnNext.Enabled = true;
+                this.btnPrevious.Enabled = true;
+                this.btnLast.Enabled = true;
+                if (pptApplication != null)
+                {
+
+                    // Get Presentation Object 
+                    presentation = pptApplication.Presentations.Open("C:\\CROSSWAY\\12step_leaf_tree.ppt", MsoTriState.msoTrue,MsoTriState.msoFalse, MsoTriState.msoFalse);
+
+                    // Get Slide collection object 
+                    slides = presentation.Slides;
+                    // Get Slide count 
+                    slidescount = slides.Count;
+                    // Get current selected slide  
+                    try
+                    {
+                        // Get selected slide object in normal view 
+                        slide = presentation.Slides.FindBySlideID(slides[1].SlideID);
+                        presentation.SlideShowSettings.Run();
+                    }
+                    catch
+                    {
+                        // Get selected slide object in reading view 
+                        slide = pptApplication.SlideShowWindows[1].View.Slide;
+                    }
+                }
+
+            }
+            catch
+            {
+                MessageBox.Show("Please Run PowerPoint Firstly", "Error", MessageBoxButtons.OKCancel, MessageBoxIcon.Error);
+            } 
+
+
         }
     }
 }
